f05a9afb8c08719aad4096cab4e46ca5e5ee744d
Guillermo <guillermonicolasromagnoli@gmail.com>
2026-05-14 15:57:27 -0300

    configurando el cors

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors"
 import router from "./routes/pedidos.routes";
+import {corsConfig} from "./config/cors";
+
 
 dotenv.config();
 const app = express();
 
 app.use(express.json());
+
+app.use(cors(corsConfig))
 
 app.use("/pedidos", router);
 
---
+++ src/config/cors.ts
@@ -0,0 +1,18 @@
+import { CorsOptions } from "cors";
+
+
+export const corsConfig : CorsOptions= {
+    origin: function( origin, callback) {
+        const whiteList=[process.env.FRONTEND_URL]
+        if (process.argv[2]==='--api'){
+            whiteList.push(undefined)
+        }
+        if (whiteList.includes(origin)){
+            callback(null, true)
+            console.log(whiteList)
+        }else{
+            callback(new Error("CorsError"))
+        }
+                    
+    }
+}
