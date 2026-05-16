8ca7a24c74e9f99bc244e2488bd73be26482e9b8
Hector Diaz <hrdr1995@gmail.com>
2026-05-16 17:35:57 -0300

    actualizando

--- src/app.ts
+++ src/app.ts
@@ -8,9 +8,9 @@ import { corsConfig } from "./config/cors";
 dotenv.config();
 const app = express();
 
+app.use(cors(corsConfig));
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(cors(corsConfig));
 
 app.use("/app", router);
 
--- src/config/cors.ts
+++ src/config/cors.ts
@@ -3,10 +3,14 @@ import { CorsOptions } from "cors";
 export const corsConfig: CorsOptions = {
   origin: function (origin, callback) {
     const whitelist = [process.env.FRONTEND_URL];
-    if (process.argv[2] === "--api") whitelist.includes(undefined);
+    console.log("origin recibido:", origin);
+    console.log(process.argv);
+    {
+      console.log("whitelist:", whitelist);
+      if (process.argv[2] === "--api") whitelist.push(undefined);
+    }
     if (whitelist.includes(origin)) {
       callback(null, true);
-      console.log(whitelist);
     } else {
       callback(new Error("CORS's Error"));
     }
