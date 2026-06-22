85927539c7eee8855c098519e68dad115e908295
Leandro Lambardi <leandrolambardi@hotmail.com>
2026-06-22 07:22:38 -0300

    feat(plato-service): add update, delete and bulk delete methods

--- src/services/platoServices.ts
+++ src/services/platoServices.ts
@@ -7,10 +7,27 @@ export class PlatoService {
   };
 
   static getPlatoById = async (id: Plato["id"]) => {
-    return await prisma.plato.findUnique({ where: { id } });
+    return await prisma.plato.findUnique({
+      where: { id },
+      include: { pedidos: true },
+    });
   };
 
   static getPlatos = async () => {
-    return await prisma.plato.findMany();
+    return await prisma.plato.findMany({
+      include: { pedidos: true },
+    });
+  };
+
+  static updatePlato = async (id: Plato["id"], data: Prisma.PlatoUpdateInput) => {
+    return await prisma.plato.update({ where: { id }, data });
+  };
+
+  static deletePlato = async (id: Plato["id"]) => {
+    return await prisma.plato.delete({ where: { id } });
+  };
+
+  static deleteMultiplePlatos = async (ids: Plato["id"][]) => {
+    return await prisma.plato.deleteMany({ where: { id: { in: ids } } });
   };
 }
